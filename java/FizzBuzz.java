4ee5afcaca76de037bf5ec01831d3a0d8f7d5ed9
j-stoff <jstoffregen@madisoncollege.edu>
2017-12-22 19:53:25 -0600

    Lessons 2 and 3 finalized

---
+++ java/FizzBuzz.java
@@ -0,0 +1,14 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        // Add code here
+        // Comments are done by using // For single line
+        /*
+            Multiline comment
+            Start at 0, go to 99
+            Each interval of 3 print Fizz
+            Each interval of 5 print Buzz
+            Intervals of both 3 and 5 print FizzBuzz (same line)
+            Also, each iteration, print the current index of the loop
+        */
+    }
+}
---
+++ java/LessonTwo.java
@@ -0,0 +1,5 @@
+public class LessonTwo {
+    public static void main(String[] args) {
+        //Code goes here
+    }
+}
